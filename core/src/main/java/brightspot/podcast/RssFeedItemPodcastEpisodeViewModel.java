1e09b79b8baa6fff520873cbed8349bef7f1eb2b
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2025-05-20 18:57:04 -0400

    Fix compilation

--- core/src/main/java/brightspot/podcast/RssFeedItemPodcastEpisodeViewModel.java
+++
@@ -1,7 +0,0 @@
-package brightspot.podcast;
-
-import brightspot.rss.feed.RssFeedItemViewModel;
-
-public class RssFeedItemPodcastEpisodeViewModel extends RssFeedItemViewModel<AbstractPodcastEpisodePage> {
-
-}
--- core/src/main/java/brightspot/podcast/RssFeedPodcastViewModel.java
+++
@@ -1,7 +0,0 @@
-package brightspot.podcast;
-
-import brightspot.rss.feed.RssFeedViewModel;
-
-public class RssFeedPodcastViewModel extends RssFeedViewModel<PodcastPage> {
-
-}
